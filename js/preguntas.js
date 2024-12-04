d89c7b4bb03be48eb1be5a7cdae9d579e9089710
dbvd28 <60490932+dbvd28@users.noreply.github.com>
2024-12-04 00:31:53 -0600

    Se agrega loading screen para el boton de agendar cita y se escribe las preguntas frecuentes

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,11 +1,3 @@
-/*function myFunction() {
-    var x = document.getElementById("info_pregunta_uno");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }*/
     function pregunta_active(){
         document.querySelectorAll('.info_button').forEach(button => {
             button.addEventListener('click', (e)=> {
