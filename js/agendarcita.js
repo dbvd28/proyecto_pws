ac3825468f5614836b0db1b43a82ce13598aa3f6
dbvd28 <60490932+dbvd28@users.noreply.github.com>
2024-12-06 19:55:24 -0600

    se corrigio agendarcita

--- js/agendarcita.js
+++ js/agendarcita.js
@@ -4,8 +4,30 @@ function setupLoadingScreen() {
     const confirmButton = document.getElementById("appoint");
     const closeCheckbox = document.getElementById("check");
     if (loadingScreen && confirmButton ) {
-        // Show the loading screen when confirmButton is clicked
+
         confirmButton.addEventListener("click", () => {
+          const nombre = document.getElementById("nombre").value.trim();
+          const email = document.getElementById("email").value.trim();
+          const number = document.getElementById("number").value.trim();
+          const date = document.getElementById("date").value.trim();
+          const time = document.getElementById("time").value.trim();
+          const description = document.getElementById("description").value.trim();
+          const missingFields = [];
+
+          // Valida si cada campo esta vacio
+          if (!nombre) missingFields.push("Nombre Completo");
+          if (!email) missingFields.push("Correo electronico");
+          if (!number) missingFields.push("Telefono");
+          if (!date) missingFields.push("Fecha");
+          if (!time) missingFields.push("Hora");
+          if (!description) missingFields.push("Motivo de la visita");
+
+          // Manda mensaje al usuario
+          if (missingFields.length > 0) {
+              alert("Por favor, complete los siguientes campos: " + missingFields.join(", "));
+              return; // Evita que se depsliegue la vsita de carga
+          }
+
             loadingScreen.classList.add("visible");
             loadingScreen.classList.remove("hidden");
         });
@@ -14,13 +36,12 @@ function setupLoadingScreen() {
             loadingScreen.classList.add("hidden");
             loadingScreen.classList.remove("visible");
         loadingScreen.classList.add("hidden");
-          }, 3000);
+          }, 5000);
       } else {
         console.error("Button or Loading Screen or Close Button not found!");
       }
   }
-  
-  // Initialize the function when the DOM is fully loaded
+x 
   document.addEventListener("DOMContentLoaded", () => {
     setupLoadingScreen();
   });
--- js/preguntas.js
+++ js/preguntas.js
@@ -1,14 +1,12 @@
     function pregunta_active(){
         document.querySelectorAll('.info_button').forEach(button => {
             button.addEventListener('click', (e)=> {
-              // Get the info from the clicked button's 'data-info' attribute
               const targetId = button.getAttribute('data-target');
          
         
               document.querySelectorAll('.contenido_pregunta').forEach(div => {
                 div.style.display = 'none';
               });
-              // Show the target div associated with the clicked button
               document.getElementById(targetId).style.display = 'block';
 
             });
